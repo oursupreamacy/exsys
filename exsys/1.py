7cbaf8c32a8446d89bedec9f45e45d4098108b19
oursupreamacy <oursupremacy931@gmail.com>
2017-11-19 21:32:17 +0300

    .

---
+++ exsys/1.py
@@ -0,0 +1,84 @@
+import sys
+import re
+
+#                   Пресеты
+preset_add = re.compile('^[(]\w+[,]\s\w+[)]$')  # (Steve, Apples)
+preset_search = re.compile('^[(]\w[,]\s\w+[)]$')  # (X, Apples)
+preset_search2 = re.compile('^[(]\w+[,]\s\w[)]$')  # (Steve, X)
+
+
+def add(string):
+    with open('База_знаний.txt', 'a') as bz:
+        bz.write(string + '\n')
+
+
+def search(string):
+    with open('База_знаний.txt', 'r') as bz:
+        for line in bz:
+            if string[2:] in line:
+                splited = line.split(',')
+                stripped = splited[0]
+                a = stripped.strip()
+                b = a.strip('(')
+                print(b)
+
+
+def search2(string):
+    with open('База_знаний.txt', 'r') as bz:
+        for line in bz:
+            if string[:2] in line:
+                splited = line.split(',')
+                stripped = splited[1]
+                a = stripped.strip()
+                b = a.strip(')')
+                print(b)
+
+
+def condition(cond, res):
+    reslist = []
+    with open('База_знаний.txt', 'r') as bz:
+        for line in bz:
+            if cond[:2] in line:
+                splited = line.split(',')
+                stripped = splited[1]
+                a = stripped.strip()
+                b = a.strip(')')
+                reslist.append(b)
+    with open('База_знаний.txt', 'a') as bz:
+        beginning = res[:4]
+        for item in reslist:
+            bz.write(beginning + ',' + ' ' + item + ')' + '\n')
+            print(beginning + ',' + ' ' + item + ')')
+
+
+def condition2(cond, res):
+    with open('База_знаний.txt', 'r+') as bz:
+        for line in bz:
+            if cond in line:
+                bz.write(res)
+
+
+def condition3(cond, res):
+    with open('База_знаний.txt', 'r+') as bz:
+        for line in bz:
+            if cond[2:] in line:
+                splited = line.split(',')
+                bz.write(splited[0] + res[2:] + '\n')
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'add' and preset_add.match(sys.argv[2]):
+        add(sys.argv[2])
+    elif sys.argv[1] == 'add' and not preset_add.match(sys.argv[2]):
+        print('Неверно введен предикат')
+    elif sys.argv[1] == 'search' and preset_search.match(sys.argv[2]):
+        search(sys.argv[2])
+    elif sys.argv[1] == 'search' and preset_search2.match(sys.argv[2]):
+        search2(sys.argv[2])
+    elif sys.argv[1] == 'if' and preset_search.match(sys.argv[2]) and preset_search.match(sys.argv[3]):
+        condition3(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'if' and preset_search2.match(sys.argv[2]) and preset_search2.match(sys.argv[3]):
+        condition(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'if' and preset_add.match(sys.argv[2]) and preset_add.match(sys.argv[3]):
+        condition2(sys.argv[2], sys.argv[3])
+    
